ec510d4e76b7c55fe444569b5ca92e0320f589d2
abdmz162 <abdmz.uetian23@gmail.com>
2025-06-14 11:34:02 +0500

    lab01 done by abdmz

--- lab01/exercise3/linked_list.c
+++ lab01/exercise3/linked_list.c
@@ -8,6 +8,7 @@ Node *create_node(int data) {
     struct Node *new_node = malloc(sizeof(struct Node));
     if (new_node == NULL) {
         perror("Malloc failed\n");
+        exit(EXIT_FAILURE);  
     }
     new_node->data = data;
     new_node->next = NULL;
@@ -55,7 +56,7 @@ void print_list(struct Node *head) {
 
 /* Iteratively reverses a linked list whose first node is HEAD */
 void reverse_list(struct Node **head) {
-    if (head == NULL) {
+    if (head == NULL || *head == NULL) {
         return;
     }
     struct Node *curr = *head;
@@ -73,13 +74,15 @@ void reverse_list(struct Node **head) {
 /* Creates a new node with a data field set to DATA and adds the node
    to the back of the list pointed to by HEAD */
 void add_to_back(Node **head, int data) {
-    if (head == NULL) {
+    Node *new_node = create_node(data);
+    if (*head == NULL) {
+        *head = new_node;
         return;
     }
-    Node *new_node = create_node(data);
-    Node *prev;
-    for (Node *curr = *head; curr != NULL; curr = curr->next) {
-        prev = curr;
+    for (Node *curr = *head; (curr != NULL); curr = curr->next) {
+        if (curr->next == NULL) {
+            curr->next = new_node;
+            break;
+        }    
     }
-    prev->next = new_node;
 }
--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,33 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
+    if(head==NULL) {
+        return 0; // No cycle in an empty list
+    }
+    node *slow = head;
+    node *fast = head;
+    fast = fast->next;
+    // if(fast == NULL) {
+    //     return 0; // No cycle if fast pointer reaches the end
+    // }else{
+    //     fast=fast->next;
+    //     if(fast==NULL){
+    //         return 0;
+    //     }
+    // }
+    // slow=slow->next;
+    while(slow != NULL && fast != NULL) {
+        if(slow == fast) {
+            return 1; // Cycle detected
+        }
+        slow = slow->next;
+        fast = fast->next;
+        if(fast != NULL) {
+            if (fast->next == NULL) {
+                return 0; // No cycle if fast pointer reaches the end
+            }
+            fast = fast->next; // Move fast pointer two steps
+        }
+    }
     /* TODO: Implement ll_has_cycle */
 }
